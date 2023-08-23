708fdc4b3bae17e75d337f1892cb1d1d93c5706a
Davi <daviibrito111@gmail.com>
2023-08-23 12:45:38 -0300

    chore: add path alias

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,6 @@
 import { Config } from 'jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
 
 const config: Config = {
   moduleFileExtensions: ['js', 'json', 'ts'],
@@ -9,6 +11,9 @@ const config: Config = {
   collectCoverageFrom: ['**/*.(t|j)s'],
   coverageDirectory: '../coverage',
   testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
 };
 
 export default config;
